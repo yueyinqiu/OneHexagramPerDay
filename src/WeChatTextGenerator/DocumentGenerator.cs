0a1e21fb30f5c36dd044d17aea273d24618dddca
yueyinqiu <lujiaqi2003@126.com>
2023-08-02 13:50:22 +0800

    do copy

---
+++ src/WeChatTextGenerator/DocumentGenerator.cs
@@ -0,0 +1,75 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using OneHexagramPerDayCore;
+using YiJingFramework.Annotating.Zhouyi;
+using YiJingFramework.Annotating.Zhouyi.Entities;
+using YiJingFramework.EntityRelationships.MostAccepted.GuaToCharacterExtensions;
+using YiJingFramework.PrimitiveTypes.GuaWithFixedCount;
+
+namespace WeChatTextGenerator;
+
+internal partial class DocumentGenerator
+{
+    private readonly ZhouyiStoreWithLineTitles zhouyi;
+    private readonly DirectoryInfo directory;
+
+    internal DocumentGenerator(DirectoryInfo directory, ZhouyiStore zhouyi)
+    {
+        this.directory = directory;
+        this.zhouyi = new(zhouyi);
+    }
+
+    private static void AppendLine(Body body, string? text = null)
+    {
+        var paragraph = body.AppendChild(new Paragraph());
+        var run = paragraph.AppendChild(new Run());
+        var properties = run.AppendChild(new RunProperties());
+        _ = properties.AppendChild(new FontSize()
+        {
+            Val = "26"
+        });
+        if (text is not null)
+            _ = run.AppendChild(new Text(text));
+    }
+
+    public void GenerateFor(GuaHexagram gua, Lunar.Lunar lunar)
+    {
+        var lunarString = $"{lunar.YearInGanZhi}年{lunar.MonthInChinese}月{lunar.DayInChinese}";
+
+        ZhouyiHexagram hexagram = this.zhouyi[gua];
+        var (upperPainting, lowerPainting) = hexagram.SplitToTrigrams();
+        ZhouyiTrigram upper = this.zhouyi.InnerStore.GetTrigram(upperPainting);
+        ZhouyiTrigram lower = this.zhouyi.InnerStore.GetTrigram(lowerPainting);
+
+        var hexagramChar = gua.ToUnicodeChar();
+        string title;
+        if (upperPainting == lowerPainting)
+            title = $"{hexagram.Name}为{upper.Nature} {lunarString}";
+        else
+            title = $"{upper.Nature}{lower.Nature}{hexagram.Name} {lunarString}";
+        var fileName = Path.GetFullPath($"{hexagramChar} {title}.docx", this.directory.FullName);
+
+        this.directory.Create();
+        using var word = WordprocessingDocument.Create(fileName, WordprocessingDocumentType.Document);
+
+        var document = new Document();
+        word.AddMainDocumentPart().Document = document;
+        var body = document.AppendChild(new Body());
+
+        AppendLine(body, hexagram.Text);
+        AppendLine(body, hexagram.Xiang);
+        AppendLine(body, hexagram.Tuan);
+        AppendLine(body);
+        foreach (var line in hexagram.EnumerateLines())
+        {
+            if (line.LineText is not null)
+            {
+                AppendLine(body, line.LineText);
+                AppendLine(body, line.Xiang);
+            }
+        }
+
+        word.Save();
+    }
+}
--- src/WeChatTextGenerator/Program.cs
+++ src/WeChatTextGenerator/Program.cs
@@ -1,104 +1,36 @@
-﻿using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using OneHexagramPerDayCore;
+﻿using OneHexagramPerDayCore;
 using System.Diagnostics;
-using System.Text;
+using TextCopy;
+using WeChatTextGenerator;
 using YiJingFramework.Annotating.Zhouyi;
-using YiJingFramework.Annotating.Zhouyi.Entities;
-using YiJingFramework.EntityRelationships.MostAccepted.GuaToCharacterExtensions;
-using YiJingFramework.PrimitiveTypes.GuaWithFixedCount;
 
-var current = DateTime.Now;
+var outputDirectory = new DirectoryInfo("./out");
+Console.WriteLine(
+    $"请注意，在生成后，剪切板的内容会被替换为要使用的原文链接。" +
+    $"同时，输出文件夹 {outputDirectory.FullName} 中的内容会被删除。");
 
-static DateOnly ParseToDateTime(string s, string? format = null)
+var currentDate = DateOnly.FromDateTime(DateTime.Now);
+Console.Write($"起始日期（默认为 {currentDate:yyyyMMdd} ）：");
+var startingDateInput = Console.ReadLine();
+if (!string.IsNullOrWhiteSpace(startingDateInput))
+    currentDate = DateOnly.ParseExact(startingDateInput, "yyyyMMdd");
+
+var storeContent = File.ReadAllText("./zhouyi.json");
+var zhouyi = ZhouyiStore.DeserializeFromJsonString(storeContent);
+Debug.Assert(zhouyi is not null);
+
+DocumentGenerator generator = new DocumentGenerator(outputDirectory, zhouyi);
+for (; ; )
 {
-    format = format ?? "yyyyMMdd";
-    return DateOnly.ParseExact(s, format);
-}
+    var hexagram = new HexagramProvider(currentDate).GetHexagram();
 
-var date = (args.Length is 0 ? "0" : args[0]) switch
-{
-    "1" => DateOnly.FromDateTime(current.AddDays(1)),
-    "2" => ParseToDateTime(args[1]),
-    _ => DateOnly.FromDateTime(current)
-};
+    outputDirectory.Delete(true);
+    var lunar = Lunar.Lunar.FromDate(currentDate.ToDateTime(new TimeOnly(6, 30)));
+    generator.GenerateFor(hexagram, lunar);
 
-Console.OutputEncoding = Encoding.Unicode;
+    ClipboardService.SetText($"https://onehexagramperday.nololiyt.top/?p={hexagram}");
 
-new Program(date).Run();
-
-internal partial class Program
-{
-    private readonly DateOnly date;
-    private readonly GuaHexagram hexagramPainting;
-
-    private readonly ZhouyiStoreWithLineTitles zhouyi;
-
-    internal Program(DateOnly date)
-    {
-        this.date = date;
-        this.hexagramPainting = new HexagramProvider(date).GetHexagram();
-
-        var storeContent = File.ReadAllText("./zhouyi.json");
-        var zhouyi = ZhouyiStore.DeserializeFromJsonString(storeContent);
-        Debug.Assert(zhouyi is not null);
-        this.zhouyi = new(zhouyi);
-    }
-
-    private static void AppendLine(Body body, string? text = null)
-    {
-        var paragraph = body.AppendChild(new Paragraph());
-        var run = paragraph.AppendChild(new Run());
-        var properties = run.AppendChild(new RunProperties());
-        _ = properties.AppendChild(new FontSize()
-        {
-            Val = "26"
-        });
-        if (text is not null)
-            _ = run.AppendChild(new Text(text));
-    }
-
-    internal void Run()
-    {
-        var lunar = Lunar.Lunar.FromDate(this.date.ToDateTime(new TimeOnly(6, 30)));
-        var lunarStr = $"{lunar.YearInGanZhi}年{lunar.MonthInChinese}月{lunar.DayInChinese}";
-        Console.WriteLine($"{this.date:yyyy/MM/dd} {lunarStr}");
-
-        ZhouyiHexagram hexagram = this.zhouyi[this.hexagramPainting];
-        var (upperPainting, lowerPainting) = hexagram.SplitToTrigrams();
-        ZhouyiTrigram upper = this.zhouyi.InnerStore.GetTrigram(upperPainting);
-        ZhouyiTrigram lower = this.zhouyi.InnerStore.GetTrigram(lowerPainting);
-
-        var hexagramChar = this.hexagramPainting.ToUnicodeChar();
-        string title;
-        if (upperPainting == lowerPainting)
-            title = $"{hexagram.Name}为{upper.Nature} {lunarStr}";
-        else
-            title = $"{upper.Nature}{lower.Nature}{hexagram.Name} {lunarStr}";
-
-        var wordFileInfo = new FileInfo($"./out/{hexagramChar} {title}.docx");
-        wordFileInfo.Directory?.Create();
-        using var word = WordprocessingDocument.Create(
-            wordFileInfo.FullName, WordprocessingDocumentType.Document);
-        var document = new Document();
-        word.AddMainDocumentPart().Document = document;
-        var body = document.AppendChild(new Body());
-
-        AppendLine(body, hexagram.Text);
-        AppendLine(body, hexagram.Xiang);
-        AppendLine(body, hexagram.Tuan);
-        AppendLine(body);
-        foreach (var line in hexagram.EnumerateLines())
-        {
-            if (line.LineText is not null)
-            {
-                AppendLine(body, line.LineText);
-                AppendLine(body, line.Xiang);
-            }
-        }
-        AppendLine(body, $"https://onehexagramperday.nololiyt.top/?p={this.hexagramPainting}");
-
-        Console.WriteLine("完成");
-    }
+    Console.Write("生成完毕，回车以继续：");
+    _ = Console.ReadLine();
+    currentDate = currentDate.AddDays(1);
 }
